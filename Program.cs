99b2792b51317a422457c2f46eb10c27d2373ff9
clydeatmcm <ccrbalaman@mcm.edu.ph>
2024-09-01 12:04:41 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,8 +24,11 @@ namespace Lastname_GP1_ATMConsoleApp_Procedural
             Console.WriteLine("*--------------------------*");
             Console.WriteLine("| Lastname ATM Secure Menu |");
             Console.WriteLine("|                          |");
-            Console.WriteLine("| 1. Insert ATM Card       |");
-            Console.WriteLine("| 2. Exit                  |");
+            Console.WriteLine("| 1. Balance Enquiry       |");
+            Console.WriteLine("| 2. Cash Deposit          |");
+            Console.WriteLine("| 3. Withdrawal            |");
+            Console.WriteLine("| 4. Transactions          |");
+            Console.WriteLine("| 5. Logout                |");
             Console.WriteLine("|                          |");
             Console.WriteLine("*--------------------------*");
         }
